76b85ea431190c91c90e019239f146ae647ec20b
Henrique-Arau <penielh43@gmail.com>
2023-12-07 11:47:58 -0300

    Convertendo o modelo do pokeApi para um modelo unico

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,26 +5,21 @@ function convertPokemonTypesToLi(pokemonTypes) {
 
 function convertPokemonToLi(pokemon) {
     return `
-        <li class="pokemon">
-            <span class="number">#${pokemon.order}</span>
+        <li class="pokemon" ${pokemon.type}>
+            <span class="number">#${pokemon.number}</span>
             <span class="name">${pokemon.name}</span>
 
             <div class="detail">
             <ol class="types">
-                ${convertPokemonTypesToLi(pokemon.types).join('')}
+                ${pokemon.types.map((type) => `<li class="type">${type}</li>`).join('')}
             </ol>
-            <img src="${pokemon.sprites.other.dream_world.front_default}" 
-            alt="${pokemon.name}">
-
-            </div>
-        
-        </li>
-    
+            <img src="${pokemon.photo}" 
+                alt="${pokemon.name}">
+            </div>    
+        </li> 
  `
 }
-
 const pokemonList = document.getElementById('pokemonList');
-
 
 pokeApi.getPokemons().then((pokemons = []) => {
     const newHtml = pokemons.map(convertPokemonToLi).join('')
